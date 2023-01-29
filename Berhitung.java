0037ed881a5a6f2700c94c69c311c9a261e5d331
RezaAziiz <117805571+RezaAziiz@users.noreply.github.com>
2023-01-29 20:57:30 +0700

    Update Berhitung.java

--- Berhitung.java
+++ Berhitung.java
@@ -5,38 +5,38 @@ public class Berhitung {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Scanner in = new Scanner(System.in); 
+		Scanner in = new Scanner(System.in); //scanner untuk input 
 		
-		int x = in.nextInt();
-		char operator = in.next().charAt(0);
-		int y = in.nextInt();
+		int x = in.nextInt();//untuk input nilai integer
+		char operator = in.next().charAt(0);//untuk input character yg akan menjadi operator
+		int y = in.nextInt();//untuk input nilai integer
 		
 		
 		
 	    
-	     
+	     //operator tambah
 	    if(operator=='+') {
 	    	int z = x + y;
 	    	 System.out.println(z);
-	    }
+	    }//
 	    else if(operator=='-') {
-	    	int z = x - y;
+	    	int z = x - y;//operator kurang
 	    	 System.out.println(z);
 	    }
 	    else if(operator=='*') {
-	    	int z = x * y;
+	    	int z = x * y;//operator kali
 	    	 System.out.println(z);
 	    }
 	    else if(operator=='/') {
-	    	int z = x/y;
+	    	int z = x/y;//operator bagi/div
 	    	 System.out.println(z);
 	    }
-	    else if(operator=='%') {
+	    else if(operator=='%') {//operator mod
 	    	int z = x%y;
 	    	 System.out.println(z);
 	    }
 	    
-	    //System.out.println(z);
+	    
 	    
 		
 	}
