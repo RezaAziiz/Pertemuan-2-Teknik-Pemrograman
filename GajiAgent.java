6f5bc071ae84921561e90c687a195cb8054f93c0
RezaAziiz <117805571+RezaAziiz@users.noreply.github.com>
2023-01-29 21:02:58 +0700

    Update GajiAgent.java

--- GajiAgent.java
+++ GajiAgent.java
@@ -5,24 +5,24 @@ public class GajiAgent {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-			Scanner in = new Scanner(System.in);
-			
+			Scanner in = new Scanner(System.in);//in untuk inputan
+			//dekalarasi dan inisialisasi variabel
 			int jumlahPenjualan;
 			int gajiBersih=500000;
 			int hargaItem=50000;
 			int totalPenjualan;
 			int gajiBonus;
 			
-			jumlahPenjualan = in.nextInt();
-			
+			jumlahPenjualan = in.nextInt();//inpujt jumlah penjumlahan
+			//jika penjumlahan >=40
 			if(jumlahPenjualan>=40){	
 			totalPenjualan = hargaItem*jumlahPenjualan;
 			 gajiBonus = gajiBersih + (totalPenjualan*25/100);
-			}	
+			}	//jika penjumlahan >80
 			else if(jumlahPenjualan>80){	
 			totalPenjualan = hargaItem*jumlahPenjualan;
 			gajiBonus = gajiBersih + (totalPenjualan*35/100);
-			}
+			}//jika penjumlahan <15
 			else if(jumlahPenjualan<15){
 			jumlahPenjualan = 15-jumlahPenjualan;
 			totalPenjualan = hargaItem*jumlahPenjualan;
