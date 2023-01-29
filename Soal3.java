d2800f48fafbe2dd72145f8c3fe7ad4012355216
RezaAziiz <117805571+RezaAziiz@users.noreply.github.com>
2023-01-29 21:04:15 +0700

    Update Soal3.java

--- Soal3.java
+++ Soal3.java
@@ -8,8 +8,8 @@ public class Soal3 {
 	Scanner keyboard = new Scanner(System.in);
 	
 	String kalimat = keyboard.nextLine();
-    String[] space = kalimat.split("[ , !, ?, ', :, ]");
-    System.out.println(space.length);
+    String[] space = kalimat.split("[ , !, ?, ', :, ]");//untuk mensplit string jika bertemu salah satu token
+    System.out.println(space.length);//untuk menentukan length dari variabel space
     for(String hasil : space) {
     	System.out.println(hasil);
     }
